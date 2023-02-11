a030f91d3dbc7c222c1bbd252f56df27d0d493d2
NITHISH0403 <nithish.kumar@tringapps.com>
2023-02-11 11:36:30 +0530

    Initial commit

---
+++ src/main/java/org/example/Contacts.java
@@ -0,0 +1,115 @@
+package org.example;
+
+import java.util.Scanner;
+
+class Node{
+    public Node next;
+    public String Name;
+    public  long Phone;
+    public String Email;
+
+    public Node(String nm, long ph, String eid){
+        Name = nm;
+        Phone = ph;
+        Email = eid;
+        next = null;
+    }
+}
+public class Contacts{
+    Node head;
+    int size;
+    Contacts(){
+        head = null;
+        size = 0;
+    }
+    void add(String nm, long ph, String eid){
+        Node node = new Node(nm, ph, eid);
+        Node current;
+        if(head == null){
+            head = node;
+        }
+        else{
+            current = head;
+            while (current.next != null){
+                current = current.next;
+            }
+            current.next = node;
+        }
+        size++;
+    }
+    void insert(String nm, long ph, String eid, int index){
+        Node node = new Node(nm, ph, eid);
+        Node current, prev = null;
+        if(index == 0){
+            current = node;
+            current.next = head;
+            head = current;
+        }
+        else if(index > 0){
+            current = head;
+            for (int i =0; i<index; i++){
+                prev = current;
+                current = current.next;
+            }
+            node.next = current;
+            prev.next = node;
+        }
+        size++;
+    }
+    void remove(int index){
+        Node current = head;
+        Node prev = null;
+        if(index == 0){
+            head = current.next;
+        }
+        else if (index > 0) {
+            for(int i = 0; i<index; i++){
+                prev = current;
+                current = current.next;
+            }
+            prev.next = current.next;
+        }
+        size--;
+    }
+    void search(String nm){
+        Node current = head;
+        String str = "";
+        int flag = 0;
+        while (current != null){
+            if(current.Name .equals(nm))
+                str += current.Name + " " + current.Phone + " " + current.Email + " ";
+                flag = 1;
+            current = current.next;
+        }
+        if(flag == 1) {
+            System.out.println("Your Search Contact is Found...");
+            System.out.println(str);
+        }
+        else {
+            System.out.println("Your Search Contact is Not Found...");
+        }
+    }
+    void print(){
+        Node current = head;
+        String str = "";
+        while (current != null){
+            str += current.Name + " " + current.Phone + " " + current.Email + " ";
+            current = current.next;
+        }
+        System.out.println(str);
+    }
+    public static void main(String[] args) {
+        Contacts c = new Contacts();
+        Scanner s = new Scanner(System.in);
+        c.add("NITHISH", 9597512667L, "nithish@gmail.com");
+        c.add("MANI", 4930872348L, "mani@gmail.com");
+        c.add("KANNAN", 8238940349L, "kannan@gmail.com");
+        c.print();
+        c.insert("KUMAR", 8912749192L, "kumar@gmail.com", 0);
+        c.print();
+        c.remove(2);
+        System.out.println("Enter the Search Contact Name:");
+        String find = s.nextLine();
+        c.search(find);
+    }
+}
